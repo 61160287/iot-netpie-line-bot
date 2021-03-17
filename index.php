9e8226881b4fd1d662ab708aa6cd21eb29b78d5f
61160287 <70052050+61160287@users.noreply.github.com>
2021-03-17 19:11:57 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
-<?php
-echo "IOT netpie LINEBot API by Tippawan Aiemsaad.";
-echo $url = 'https://covid19.ddc.moph.go.th';
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>HTML Links</h1>
+
+<p><a href="https://covid19.ddc.moph.go.th/">รายงานสถานการณ์ โควิด-19</a></p>
+
+</body>
+</html>
