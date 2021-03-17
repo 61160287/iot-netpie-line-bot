5f94a6ce0f4d81e0acd2963907126fb60833eabd
61160287 <70052050+61160287@users.noreply.github.com>
2021-03-17 19:13:08 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
 <h1>HTML Links</h1>
 
-<p><a href="https://covid19.ddc.moph.go.th/">รายงานสถานการณ์ โควิด-19</a></p>
+<p><a href="https://covid19.ddc.moph.go.th">รายงานสถานการณ์ โควิด-19</a></p>
 
 </body>
 </html>
